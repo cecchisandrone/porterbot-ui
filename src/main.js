e8621141811260903984ccc45d87dcbc17995c8a
Alessandro Dionisi <cecchisandrone@gmail.com>
2018-02-02 09:41:03 +0100

    Revert

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import VueSocketIO from 'vue-socket.io';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-Vue.use(VueSocketIO, 'http://porterbot-controller:8080')
+Vue.use(VueSocketIO, 'http://porterbot:8080')
 
 new Vue({
   el: '#app',
